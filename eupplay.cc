2546cf6ea26abf9f573ac623bade41cb06475f1a
Tomoaki Hayasaka <hayasaka@malmodeler.com>
2023-07-28 08:36:02 +0900

    import eupplayer-0.008

---
+++ eupplay.cc
@@ -0,0 +1,299 @@
+/*      Copyright (C) 1995-1996, 2000 Tomoaki HAYASAKA.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ */
+
+// $Id: eupplay.cc,v 1.15 2000/04/12 23:20:56 hayasaka Exp $
+
+#include "eupplayer.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string>
+#include <vector>
+#include <strstream.h>
+using namespace std;
+#include "eupplayer_townsEmulator.h"
+
+static string const downcase(string const &s)
+{
+  string r;
+  for (string::const_iterator i = s.begin(); i != s.end(); i++)
+    r += (*i >= 'A' && *i <= 'Z') ? (*i - 'A' + 'a') : *i;
+  return r;
+}
+
+static string const upcase(string const &s)
+{
+  string r;
+  for (string::const_iterator i = s.begin(); i != s.end(); i++)
+    r += (*i >= 'a' && *i <= 'z') ? (*i - 'a' + 'A') : *i;
+  return r;
+}
+
+static FILE *openFile_inPath(string const &filename, string const &path)
+{
+  FILE *f = NULL;
+  vector<string> fn2;
+  fn2.push_back(filename);
+  fn2.push_back(downcase(filename));
+  fn2.push_back(upcase(filename));
+
+  string::const_iterator i = path.begin();
+  while (i != path.end()) {
+    string dir;
+    while (i != path.end() && *i != ':')
+      dir += *i++;
+    if (i != path.end() && *i == ':')
+      i++;
+    for (vector<string>::const_iterator j = fn2.begin(); j != fn2.end(); j++) {
+      string const filename(dir + "/" + *j);
+      //cerr << "trying " << filename << endl;
+      f = fopen(filename.c_str(), "r");
+      if (f != NULL) {
+	//cerr << "loading " << filename << endl;
+	return f;
+      }
+    }
+  }
+  if (f == NULL)
+    fprintf(stderr, "error finding %s\n", filename.c_str());
+  
+  return f;
+}
+
+u_char *EUPPlayer_readFile(EUPPlayer *player,
+			   TownsAudioDevice *device,
+			   string const &nameOfEupFile)
+{
+  // とりあえず, TOWNS emu のみに対応.
+  
+  u_char *eupbuf = NULL;
+  player->stopPlaying();
+  
+  {
+    FILE *f = fopen(nameOfEupFile.c_str(), "r");
+    if (f == NULL) {
+      perror(nameOfEupFile.c_str());
+      return NULL;
+    }
+    
+    struct stat statbuf;
+    if (fstat(fileno(f), &statbuf) != 0) {
+      perror(nameOfEupFile.c_str());
+      fclose(f);
+      return NULL;
+    }
+
+    if (statbuf.st_size < 2048 + 6 + 6) {
+      fprintf(stderr, "%s: too short file.\n", nameOfEupFile.c_str());
+      fclose(f);
+      return NULL;
+    }
+    
+    eupbuf = new u_char[statbuf.st_size];
+    fread(eupbuf, statbuf.st_size, 1, f);
+    fclose(f);
+  }
+
+  player->tempo(eupbuf[2048 + 5] + 30);
+  // 初期テンポの設定のつもり.  これで正しい?
+
+  for (int n = 0; n < 32; n++)
+    player->mapTrack_toChannel(n, eupbuf[0x394 + n]);
+
+  for (int n = 0; n < 6; n++)
+    device->assignFmDeviceToChannel(eupbuf[0x6d4 + n]);
+  for (int n = 0; n < 8; n++)
+    device->assignPcmDeviceToChannel(eupbuf[0x6da + n]);
+  
+  string fmbPath(".:/usr/local/share/fj2/tone:/usr/share/fj2/tone");
+  string pmbPath(".:/usr/local/share/fj2/tone:/usr/share/fj2/tone");
+  {
+    char *s;
+    s = getenv("EUP_FMINST");
+    if (s != NULL)
+      fmbPath = string(s);
+    s = getenv("EUP_PCMINST");
+    if (s != NULL)
+      pmbPath = string(s);
+    string eupDir = nameOfEupFile.substr(0, nameOfEupFile.rfind("/") + 1) + ".";
+    fmbPath = eupDir + ":" + fmbPath;
+    pmbPath = eupDir + ":" + pmbPath;
+  }
+  
+  {
+#if 0
+    u_char instrument[] = {
+      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', // name
+      1, 7, 1, 1,			      // detune / multiple
+      23, 40, 38, 0,			      // output level
+      140, 140, 140, 83,		      // key scale / attack rate
+      13, 13, 13, 3,			      // amon / decay rate
+      0, 0, 0, 0,			      // sustain rate
+      19, 250, 19, 10,			      // sustain level / release rate
+      58,				      // feedback / algorithm
+      0xc0,				      // pan, LFO
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    };
+#else
+    u_char instrument[] = {
+      ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', // name
+      17, 33, 10, 17,			      // detune / multiple
+      25, 10, 57, 0,			      // output level
+      154, 152, 218, 216,		      // key scale / attack rate
+      15, 12, 7, 12,			      // amon / decay rate
+      0, 5, 3, 5,			      // sustain rate
+      38, 40, 70, 40,			      // sustain level / release rate
+      20,				      // feedback / algorithm
+      0xc0,				      // pan, LFO
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    };
+#endif
+    for (int n = 0; n < 128; n++)
+      device->setFmInstrumentParameter(n, instrument);
+  }
+  
+  {
+    char fn0[16];
+    memcpy(fn0, eupbuf + 0x6e2, 8);
+    fn0[8] = '\0';
+    string fn1(string(fn0) + ".fmb");
+    FILE *f = openFile_inPath(fn1, fmbPath);
+    if (f != NULL) {
+      struct stat statbuf;
+      fstat(fileno(f), &statbuf);
+      u_char buf1[statbuf.st_size];
+      fread(buf1, statbuf.st_size, 1, f);
+      fclose(f);
+      for (int n = 0; n < (statbuf.st_size - 8) / 48; n++)
+	device->setFmInstrumentParameter(n, buf1 + 8 + 48 * n);
+    }
+  }
+  
+  {
+    char fn0[16];
+    memcpy(fn0, eupbuf + 0x6ea, 8);
+    fn0[8] = '\0';
+    string fn1(string(fn0) + ".pmb");
+    FILE *f = openFile_inPath(fn1, pmbPath);
+    if (f != NULL) {
+      struct stat statbuf;
+      fstat(fileno(f), &statbuf);
+      u_char buf1[statbuf.st_size];
+      fread(buf1, statbuf.st_size, 1, f);
+      fclose(f);
+      device->setPcmInstrumentParameters(buf1, statbuf.st_size);
+    }
+  }
+
+  return eupbuf;  
+}
+
+int main(int argc, char **argv)
+{
+  EUP_TownsEmulator *dev = new EUP_TownsEmulator;
+  dev->outputStream(stdout);
+  EUPPlayer *player = new EUPPlayer();
+  player->outputDevice(dev);
+  
+  int c;
+  while ((c = getopt(argc, argv, "usbwt:r:v:d:")) != -1)
+    switch(c) {
+    case 's':
+      dev->outputSampleUnsigned(false);
+      break;
+    case 'u':
+      dev->outputSampleUnsigned(true);
+      break;
+    case 'b':
+      dev->outputSampleSize(1);
+      break;
+    case 'w':
+      dev->outputSampleSize(2);
+      break;
+    case 't':
+      {
+	string arg(optarg);
+	if (arg == ".sb") {
+	  dev->outputSampleUnsigned(false);
+	  dev->outputSampleSize(1);
+	} else if (arg == ".ub") {
+	  dev->outputSampleUnsigned(true);
+	  dev->outputSampleSize(1);
+	} else if (arg == ".sw") {
+	  dev->outputSampleUnsigned(false);
+	  dev->outputSampleSize(2);
+	  dev->outputSampleLSBFirst(true);
+	} else if (arg == ".uw") {
+	  dev->outputSampleUnsigned(true);
+	  dev->outputSampleSize(2);
+	  dev->outputSampleLSBFirst(true);
+	} else if (arg == ".ws") {
+	  dev->outputSampleUnsigned(false);
+	  dev->outputSampleSize(2);
+	  dev->outputSampleLSBFirst(false);
+	} else if (arg == ".wu") {
+	  dev->outputSampleUnsigned(true);
+	  dev->outputSampleSize(2);
+	  dev->outputSampleLSBFirst(false);
+	} else {
+	  fprintf(stderr, "unknown sample format %s, assuming .ub\n",
+		  arg.c_str());
+	  dev->outputSampleUnsigned(true);
+	  dev->outputSampleSize(1);
+	}
+      }
+      break;
+    case 'r':
+      dev->rate(strtol(optarg, NULL, 0));
+      break;
+    case 'v':
+      dev->volume(strtol(optarg, NULL, 0));
+      break;
+    case 'd':
+      {
+	istrstream str(optarg);
+	while (!str.eof() && !str.fail()) {
+	  int n;
+	  str >> n;
+	  dev->enable(n, false);
+	}
+      }
+      break;
+    }
+  
+  int optindex = optind;
+  if (optindex > argc-1) {
+    fprintf(stderr, "usage: %s [-t format] [-r rate] [-v vol] [-b|-w] [-d ch[,ch...]] EUP-filename\n", argv[0]);
+    exit(1);
+  }
+  
+  char const *nameOfEupFile = argv[optindex++];
+  u_char *buf = EUPPlayer_readFile(player, dev, nameOfEupFile);
+  if (buf == NULL) {
+    fprintf(stderr, "%s: read failed\n", argv[1]);
+    exit(1);
+  }
+  
+  player->startPlaying(buf + 2048 + 6);
+  while (player->isPlaying())
+    player->nextTick();
+  
+  delete player;
+  delete dev;
+  delete buf;
+  
+  return 0;
+}
---
+++ eupplayer.cc
@@ -0,0 +1,411 @@
+// $Id: eupplayer.cc,v 1.9 2000/04/12 23:17:18 hayasaka Exp $
+
+#include "eupplayer.h"
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include "sintbl.h"
+
+/* 0 は 0
+   1 は heplay っぽく
+   2 は 純正っぽく */
+#define OVERSTEP 2
+
+#ifdef DEBUG
+#include <stdarg.h>
+static void dbprintf(char *fmt, ...)
+{
+  va_list args;
+  va_start(args, fmt);
+  vfprintf(stderr, fmt, args);
+  va_end(args);
+  fflush(stderr);
+}
+#define DB(a) dbprintf##a
+#else
+#define DB(a)
+#endif
+
+#define DB_PROCESSING(mes) DB(("* com=%02x tr=%02x sl=%02x sh=%02x p4=%02x p5=%02x (%s)\n", \
+  (pl->_curP)[0], (pl->_curP)[1], (pl->_curP)[2], \
+  (pl->_curP)[3], (pl->_curP)[4], (pl->_curP)[5], mes))
+
+#if OVERSTEP == 2
+#define COMPRESSOVERSTEPS if (stepTime >= 384) stepTime = 383
+  /* .... >= 384 ってのは steptime >= 小節長 のつもり。
+     HEat で作られた .eup ファイルの中に存在することがある。  */
+#else
+#define COMPRESSOVERSTEPS
+#endif
+
+#define WAIT4NEXTSTEP \
+  int stepTime = (pl->_curP)[2] + 0x80*(pl->_curP)[3]; \
+  COMPRESSOVERSTEPS; \
+  if (stepTime > pl->_stepTime) return 1;
+
+
+/* EUPPlayer */
+
+EUPPlayer::EUPPlayer()
+{
+  for (int i = 0; i < _maxTrackNum; i++)
+    _track2channel[i] = 0;
+
+  _outputDev = NULL;
+  _stepTime = 0;
+  _tempo = -1;
+  this->stopPlaying();
+}
+
+EUPPlayer::~EUPPlayer()
+{
+  this->stopPlaying();
+}
+
+void EUPPlayer::outputDevice(PolyphonicAudioDevice *outputDev)
+{
+  this->stopPlaying();
+  _outputDev = outputDev;
+  this->tempo(this->tempo());
+}
+
+void EUPPlayer::mapTrack_toChannel(int track, int channel)
+{
+  assert(track >= 0);
+  assert(track < _maxTrackNum);
+  assert(channel >= 0);
+  //assert(channel < _maxChannelNum);
+
+  _track2channel[track] = channel;
+}
+
+void EUPPlayer::startPlaying(u_char const *ptr)
+{
+  _isPlaying = 0;
+  if (ptr != NULL) {
+    _curP = ptr;
+    _isPlaying = 1;
+    _stepTime = 0;
+  }
+
+#if 0
+  for (int track = 0; track < _maxTrackNum; track++)
+    _outputDev->programChange(_track2channel[track], 0);
+#endif
+}
+
+void EUPPlayer::stopPlaying()
+{
+  _isPlaying = 0;
+}
+
+int EUPPlayer::isPlaying() const
+{
+  return _isPlaying;
+}
+
+typedef int (*CommandProc)(int, EUPPlayer *);
+
+static CommandProc const _commands[0x08] = {
+  EUPPlayer_cmd_8x, EUPPlayer_cmd_9x, EUPPlayer_cmd_ax, EUPPlayer_cmd_bx,
+  EUPPlayer_cmd_cx, EUPPlayer_cmd_dx, EUPPlayer_cmd_ex, EUPPlayer_cmd_fx,	
+};
+
+static CommandProc const _fCommands[0x10] = {
+  EUPPlayer_cmd_f0, EUPPlayer_cmd_f1, EUPPlayer_cmd_f2, EUPPlayer_cmd_f3,
+  EUPPlayer_cmd_f4, EUPPlayer_cmd_f5, EUPPlayer_cmd_f6, EUPPlayer_cmd_f7,
+  EUPPlayer_cmd_f8, EUPPlayer_cmd_f9, EUPPlayer_cmd_fa, EUPPlayer_cmd_fb,
+  EUPPlayer_cmd_fc, EUPPlayer_cmd_fd, EUPPlayer_cmd_fe, EUPPlayer_cmd_ff,
+};
+
+int EUPPlayer_cmd_INVALID(int cmd, EUPPlayer *pl)
+{
+  cerr << "EUPPlayer: invalid command '" << hex << cmd << "'.\n";
+  DB(("EUPPlayer: invalid command '%02x'.\n", cmd));
+  (pl->_curP) += 6;
+  return 1;
+}
+
+int EUPPlayer_cmd_NOTSUPPORTED(int cmd, EUPPlayer *pl)
+{
+  cerr << "EUPPlayer: not supported command '" << hex << cmd << "'.\n";
+  DB(("EUPPlayer: not supported command '%02x'.\n", cmd));
+  (pl->_curP) += 6;
+  return 1;
+}
+
+int EUPPlayer_cmd_8x(int cmd, EUPPlayer *pl)
+{
+  /* ノートオフ */
+  DB_PROCESSING("Note off");
+  
+  /* 単独で来るはずが無いからエラーにする.  でも, DATA CONTINUE の直後
+     なら有り得るかな?  */
+  
+  return EUPPlayer_cmd_INVALID(cmd, pl);
+}
+
+int EUPPlayer_cmd_9x(int cmd, EUPPlayer *pl)
+{
+  /* ノートオン */
+  WAIT4NEXTSTEP;
+  DB_PROCESSING("Note on");
+
+  if (((pl->_curP)[6] & 0xf0) != 0x80) {
+    /* 次のコマンドが不正だってメッセージ出さなきゃなぁ  */
+    return EUPPlayer_cmd_INVALID(cmd, pl);
+  }
+  
+  if ((cmd & 0x0f) != 0)
+    DB(("MIDI-ch is not zero (%02x).\n", cmd)); // pb_theme など
+  
+  int track = (pl->_curP)[1];
+  int note = (pl->_curP)[4];
+  int onVelo = (pl->_curP)[5];
+  int gateTime = (0x1*(pl->_curP[7]) + 0x10*(pl->_curP[8]) +
+		  0x100*(pl->_curP[9]) + 0x1000*(pl->_curP[10]));
+  int offVelo = (pl->_curP)[11];
+  //DB(("tr %02x\n", track));
+  //DB(("step=%d, track=%d, note=%d, on=%d, gate=%d, off=%d\n", stepTime, track, note, onVelo, gateTime, offVelo));
+  if (offVelo == 0 || offVelo >= 0x80)
+    offVelo = onVelo; // これでいいのだろうか?  (灰p.437)
+  pl->_outputDev->note(pl->_track2channel[track], note, onVelo, offVelo, gateTime);
+  
+  pl->_curP += 12;
+  return 0;
+}
+
+int EUPPlayer_cmd_ax(int cmd, EUPPlayer *pl)
+{
+  /* ポリフォニックアフタータッチ */
+  DB_PROCESSING("Polyphonic after touch");
+  return EUPPlayer_cmd_NOTSUPPORTED(cmd, pl);
+}
+
+int EUPPlayer_cmd_bx(int cmd, EUPPlayer *pl)
+{
+  /* コントロールチェンジ */
+  WAIT4NEXTSTEP;
+  DB_PROCESSING("Control change");
+  int track = (pl->_curP)[1];
+  int control = (pl->_curP)[4];
+  int value = (pl->_curP)[5];
+  pl->_outputDev->controlChange(pl->_track2channel[track], control, value);
+  pl->_curP += 6;
+  return 0;
+}
+
+int EUPPlayer_cmd_cx(int cmd, EUPPlayer *pl)
+{
+  /* プログラムチェンジ */
+  WAIT4NEXTSTEP;
+  DB_PROCESSING("Program change");
+  int track = (pl->_curP)[1];
+  int num = (pl->_curP)[4];
+  pl->_outputDev->programChange(pl->_track2channel[track], num);
+  pl->_curP += 6;
+  return 0;
+}
+
+int EUPPlayer_cmd_dx(int cmd, EUPPlayer *pl)
+{
+  /* チャンネルアフタータッチ */
+  DB_PROCESSING("Channel after touch");
+  return EUPPlayer_cmd_NOTSUPPORTED(cmd, pl);
+}
+
+int EUPPlayer_cmd_ex(int cmd, EUPPlayer *pl)
+{
+  /* ピッチベンド */
+  WAIT4NEXTSTEP;
+  DB_PROCESSING("Pitch bend");
+  
+  int track = (pl->_curP)[1];
+  int value = (pl->_curP)[4] + ((pl->_curP)[5] << 7);
+  pl->_outputDev->pitchBend(pl->_track2channel[track], value);
+  
+  pl->_curP += 6;
+  return 0;
+}
+
+int EUPPlayer_cmd_fx(int cmd, EUPPlayer *pl)
+{
+  return (_fCommands[cmd & 0x0f])(cmd, pl);
+}
+
+int EUPPlayer_cmd_f0(int cmd, EUPPlayer *pl)
+{
+  /* エクスクルーシブステータス */
+  DB_PROCESSING("Exclusive status");
+  return EUPPlayer_cmd_NOTSUPPORTED(cmd, pl);
+}
+
+int EUPPlayer_cmd_f1(int cmd, EUPPlayer *pl)
+{
+  /* 未定義 */
+  DB_PROCESSING("Undefined");
+  return EUPPlayer_cmd_INVALID(cmd, pl);
+}
+
+int EUPPlayer_cmd_f2(int cmd, EUPPlayer *pl)
+{
+  /* 小節マーカー */
+  WAIT4NEXTSTEP;
+  DB_PROCESSING("Bar");
+  
+#if OVERSTEP == 0 || OVERSTEP == 2
+  pl->_stepTime = 0;
+  // 仕様通りだとこうするべきなのだと思う
+  // ただし、純正プレイヤーではさらに変なことをやってるっぽい。
+#endif
+#if OVERSTEP == 1
+  pl->_stepTime -= stepTime;
+  // こうすると heplay っぽい
+#endif
+  pl->_curP += 6;
+  return 0;
+}
+
+int EUPPlayer_cmd_f3(int cmd, EUPPlayer *pl)
+{
+  /* 未定義 */
+  DB_PROCESSING("Undefined");
+  return EUPPlayer_cmd_INVALID(cmd, pl);
+}
+
+int EUPPlayer_cmd_f4(int cmd, EUPPlayer *pl)
+{
+  /* 未定義 */
+  DB_PROCESSING("Undefined");
+  return EUPPlayer_cmd_INVALID(cmd, pl);
+}
+
+int EUPPlayer_cmd_f5(int cmd, EUPPlayer *pl)
+{
+  /* 未定義 */
+  DB_PROCESSING("Undefined");
+  return EUPPlayer_cmd_INVALID(cmd, pl);
+}
+
+int EUPPlayer_cmd_f6(int cmd, EUPPlayer *pl)
+{
+  /* 未定義 */
+  DB_PROCESSING("Undefined");
+  return EUPPlayer_cmd_INVALID(cmd, pl);
+}
+
+int EUPPlayer_cmd_f7(int cmd, EUPPlayer *pl)
+{
+  /* END OF エクスクルーシブ */
+  DB_PROCESSING("End of exclusive");
+  return EUPPlayer_cmd_NOTSUPPORTED(cmd, pl);
+}
+
+int EUPPlayer_cmd_f8(int cmd, EUPPlayer *pl)
+{
+  /* テンポ */
+  WAIT4NEXTSTEP;
+  DB_PROCESSING("Tempo");
+  int t = 30 + (pl->_curP)[4] + ((pl->_curP)[5] << 7);
+  pl->tempo(t);
+  (pl->_curP) += 6;
+  return 0;
+}
+
+int EUPPlayer_cmd_f9(int cmd, EUPPlayer *pl)
+{
+  /* 未定義 */
+  DB_PROCESSING("Undefined");
+  return EUPPlayer_cmd_INVALID(cmd, pl);
+}
+
+int EUPPlayer_cmd_fa(int cmd, EUPPlayer *pl)
+{
+  /* USER CALL PROGRAM */
+  DB_PROCESSING("User call program");
+  return EUPPlayer_cmd_NOTSUPPORTED(cmd, pl);
+}
+
+int EUPPlayer_cmd_fb(int cmd, EUPPlayer *pl)
+{
+  /* パターン番号 */
+  DB_PROCESSING("Pattern number");
+  return EUPPlayer_cmd_NOTSUPPORTED(cmd, pl);
+}
+
+int EUPPlayer_cmd_fc(int cmd, EUPPlayer *pl)
+{
+  /* TRACK COMMAND */
+  DB_PROCESSING("Track command");
+  return EUPPlayer_cmd_NOTSUPPORTED(cmd, pl);
+}
+
+int EUPPlayer_cmd_fd(int cmd, EUPPlayer *pl)
+{
+  /* DATA CONTINUE */
+  WAIT4NEXTSTEP;
+  DB_PROCESSING("Data continue");
+  pl->stopPlaying();		/* 本来は一時停止 */
+  return EUPPlayer_cmd_NOTSUPPORTED(cmd, pl);
+}
+
+int EUPPlayer_cmd_fe(int cmd, EUPPlayer *pl)
+{
+  /* 終端マーカー */
+  WAIT4NEXTSTEP;
+  DB_PROCESSING("End mark");
+  DB(("EUPPlayer: playing terminated.\n"));
+  pl->stopPlaying();
+  return 1;
+}
+
+int EUPPlayer_cmd_ff(int cmd, EUPPlayer *pl)
+{
+  /* ダミーコード */
+  DB_PROCESSING("Dummy");
+  pl->_curP += 6;
+  return 0;
+}
+
+void EUPPlayer::nextTick()
+{
+  // steptime ひとつ分進める
+  
+  if (this->isPlaying())
+    for (;;) {
+      int cmd = *_curP;
+      CommandProc cmdProc = EUPPlayer_cmd_INVALID;
+      if (cmd >= 0x80)
+	cmdProc = _commands[((cmd-0x80)>>4) & 0x07];
+      if (cmdProc(cmd, this))
+	break;
+    }
+  
+  if (_outputDev != NULL)
+    _outputDev->nextTick();
+  
+  _stepTime++;
+}
+
+int EUPPlayer::tempo() const
+{
+  return _tempo;
+}
+
+void EUPPlayer::tempo(int t)
+{
+  _tempo = t;
+  
+  if (_tempo <= 0)
+    return;
+  
+  int t0 = 96 * t;
+  struct timeval tv;
+  tv.tv_sec = 60 / t0;
+  tv.tv_usec = ((60*1000*1000) / t0) - tv.tv_sec*1000*1000;
+  if (_outputDev != NULL)
+    _outputDev->timeStep(tv);
+}
